7fddaad5b0ace908b1e9c3be7e4ee415e6123f2f
david <davidmiro15@gmail.com>
2026-05-18 17:40:45 -0400

    Initial project setup

---
+++ src/main/java/david/dev/Main.java
@@ -0,0 +1,27 @@
+package david.dev;
+
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        String word = "J@va the be$t!123";
+
+        Stack<Character> letter = new Stack<>();
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isLetter(word.charAt(i))) {
+                letter.push(word.charAt(i));
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < word.length(); i++) {
+            if (Character.isLetter(word.charAt(i))) {
+                sb.append(letter.pop());
+            } else {
+                sb.append(word.charAt(i));
+            }
+        }
+        System.out.println(sb.toString());
+    }
+}
