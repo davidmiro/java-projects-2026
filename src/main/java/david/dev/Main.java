f7e2caac8351800d6460bbeb1938fcdda4b5e19a
david <davidmiro15@gmail.com>
2026-05-21 01:22:27 -0400

    implemented string reverse algorithm using one loop.

--- src/main/java/david/dev/Main.java
+++ src/main/java/david/dev/Main.java
@@ -6,21 +6,42 @@ public class Main {
     public static void main(String[] args) {
         String word = "J@va the be$t!123";
 
-        Stack<Character> letter = new Stack<>();
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isLetter(word.charAt(i))) {
-                letter.push(word.charAt(i));
-            }
-        }
-        StringBuilder sb = new StringBuilder();
+        char[] letters = word.toCharArray();
+        int left = 0;
+        int right = letters.length - 1;
 
-        for (int i = 0; i < word.length(); i++) {
-            if (Character.isLetter(word.charAt(i))) {
-                sb.append(letter.pop());
+        while (left < right) {
+            if (!Character.isLetter(letters[left])) {
+                left++;
+            } else if (!Character.isLetter(letters[right])) {
+                right--;
             } else {
-                sb.append(word.charAt(i));
+                char temp = letters[left];
+                letters[left] = letters[right];
+                letters[right] = temp;
+                left++;
+                right--;
             }
         }
-        System.out.println(sb.toString());
+
+        System.out.println(new String(letters));
+
+
+//        Stack<Character> letter = new Stack<>();
+//        for (int i = 0; i < word.length(); i++) {
+//            if (Character.isLetter(word.charAt(i))) {
+//                letter.push(word.charAt(i));
+//            }
+//        }
+//        StringBuilder sb = new StringBuilder();
+//
+//        for (int i = 0; i < word.length(); i++) {
+//            if (Character.isLetter(word.charAt(i))) {
+//                sb.append(letter.pop());
+//            } else {
+//                sb.append(word.charAt(i));
+//            }
+//        }
+//        System.out.println(sb.toString());
     }
 }
